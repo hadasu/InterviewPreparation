43b009bb5e8a5399f526768e5291c5ff4886de34
hadasu <hadasu92@gmail.com>
2017-09-19 09:57:53 +0300

    Add project files.

---
+++ InterviewPreparation/BinaryTree.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPreparation
+{
+    class BinaryTree
+    {
+        public Node root;
+        private int count;
+
+        public BinaryTree()
+        {
+            root = null;
+            count = 0;
+        }
+        public bool isEmpty()
+        {
+            return root == null;
+        }
+
+        public void insert(int d)
+        {
+            if (isEmpty())
+            {
+                root = new Node(d);
+            }
+            else
+            {
+                root.insertData(ref root, d);
+            }
+
+            count++;
+        }
+
+        public bool search(int s)
+        {
+            return root.search(root, s);
+        }
+
+        public bool isLeaf()
+        {
+            if (!isEmpty())
+                return root.isLeaf(ref root);
+
+            return true;
+        }
+
+        public void display()
+        {
+            if (!isEmpty())
+                root.display(root);
+        }
+
+        public int Count()
+        {
+            return count;
+        }
+
+        public void ReverseTree(Node root)
+        {
+            Node tmp = root.leftLeaf;
+            root.leftLeaf = root.rightLeaf;
+            root.rightLeaf = tmp;
+
+            if (root.leftLeaf != null) ReverseTree(root.leftLeaf);
+            if (root.rightLeaf != null) ReverseTree(root.rightLeaf);
+        }
+    }
+}
---
+++ InterviewPreparation/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewPreparation
+{
+    class Program
+    {
+        static List<string> list = new List<string>();
+        static Dictionary<string, string> Dictionary = new Dictionary<string, string>();
+
+        static void Main(string[] args)
+        {
+            //DirSearch("C:\\Users\\Nikita\\Desktop\\New folder"); *1*
+            //Dictionary.Reverse();
+            //list.Reverse();
+
+            //RegularExpressions(); *2*
+
+            //BinaryTree b = new BinaryTree(); *3*
+            //b.insert(1);
+            //b.insert(6);
+            //b.insert(2);
+            //b.insert(4);
+            //b.insert(5);
+            //b.insert(3);
+            //b.ReverseTree(b.root);
+            //b.display();
+
+
+
+            Console.ReadLine();
+        }
+
+        // DirSearch 1
+        static void DirSearch(string sDir) 
+        {
+            try
+            {
+                foreach (string dir in Directory.GetDirectories(sDir))
+                {
+                    foreach (string file in Directory.GetFiles(dir))
+                    {
+                        list.Add(file);
+                        Dictionary.Add(file, file);
+                        Console.WriteLine(file);
+                    }
+                    DirSearch(dir);
+                }
+            }
+            catch (System.Exception excpt)
+            {
+                Console.WriteLine(excpt.Message);
+            }
+        }
+
+        // RegularExpressions - 2
+        // https://docs.microsoft.com/en-us/dotnet/standard/base-types/regular-expression-language-quick-reference
+        static void RegularExpressions()
+        {
+            string[] numbers =
+            {
+            "123-555-0190",
+            "444-234-22450",
+            "690-555-0178",
+            "146-893-232",
+            "146-555-0122",
+            "4007-555-0111",
+            "407-555-0111",
+            "407-2-5555",
+        };
+
+            string sPattern = "^\\d{3}-\\d{3}-\\d{4}$";
+
+            foreach (string s in numbers)
+            {
+                System.Console.Write(s);
+
+                if (System.Text.RegularExpressions.Regex.IsMatch(s, sPattern))
+                {
+                    System.Console.WriteLine(" - valid");
+                }
+                else
+                {
+                    System.Console.WriteLine(" - invalid");
+                }
+            }
+        }
+
+
+    }
+}
